7207c8ff3aef6bd3fcdbbe9ab1817bb8823f726e
chaukesonnyboy <sonnyboy99chauke@gmail.com>
2024-10-17 21:08:37 +0200

    Modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
 	return (
 		<footer className=" w-full p-4 md:py-8">
-			<span className="block text-sm text-gray-600 text-center">
-				&copy; 2024 All Rights Reserved.
+			<span className="md:block text-sm text-gray-600 text-center flex ">
+				&copy; 2024 All Rights Reserved. Developed by Sonnyboy Chauke
 			</span>
 		</footer>
 	);
