de3f96ab4ab36da2503e57e1ccd5a47d377e62f3
Sonnyboy Chauke <sonnyboy99chauke@gmail.com>
2026-05-02 17:14:35 +0200

    Update Footer component to reflect new copyright year and branding for MovieHunt.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
 	return (
-		<footer className=" w-full p-4 md:py-8">
-			<span className="md:block text-sm text-gray-600 text-center flex ">
-				&copy; 2024 All Rights Reserved. By Sonnyboy Chauke.
-			</span>
+		<footer className="w-full p-4 md:py-8">
+			<p className="text-sm text-gray-600 text-center">
+				&copy; 2026 MovieHunt. All rights reserved.
+			</p>
 		</footer>
 	);
 };
