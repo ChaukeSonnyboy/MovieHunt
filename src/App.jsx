af83b29f1ecb878ce5f5c3a9993c156440f69dab
Sonnyboy Chauke <sonnyboy99chauke@gmail.com>
2026-05-02 17:16:57 +0200

    Add ScrollToTop component to App for improved navigation experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
 import WebRoutes from "./routes/WebRoutes";
 
 const App = () => {
 	return (
 		<>
+			<ScrollToTop />
 			<Header />
 			<main className="min-h-screen">
 				<WebRoutes />
